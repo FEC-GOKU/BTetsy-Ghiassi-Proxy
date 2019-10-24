f596c01128acb042ae1d9b90415d152c3f17a372
cghiassi5 <cghiassi5@gmail.com>
2019-10-24 11:48:01 -0700

    started implementing proxy

--- server/index.js
+++ server/index.js
@@ -1,15 +1,15 @@
 const express = require('express');
 const proxy = require('http-proxy-middleware');
-const port = 1000;
+const port = 1994;
 app = express();
 
 
 app.use(proxy('/api/seller', {target: 'http://localhost:5000'}));
-app.use(proxy('/products', {target: 'http://localhost:3000'}));
+// app.use(proxy('/products', {target: 'http://localhost:3000'}));
 app.use(proxy('/api/checkout', {target: 'http://localhost:1234'}));
 app.use(proxy('/index', {target: 'http://localhost:4000'}));
 app.use(express.static('./public'));
 
-app.listen(PORT, () => {
+app.listen(port, () => {
   console.log(`listening at port ${port}:`)
 });
