c00b8ec9fbf4bad93bc00ad96d3f1991c5f7635a
cghiassi5 <cghiassi5@gmail.com>
2019-10-28 11:41:47 -0700

    made css changes

--- server/index.js
+++ server/index.js
@@ -1,13 +1,16 @@
 const express = require('express');
 const proxy = require('http-proxy-middleware');
-const port = 1994;
+const port = 4321;
 app = express();
 
-
+//seller = reviews
 app.use(proxy('/api/seller', {target: 'http://localhost:5000'}));
-// app.use(proxy('/products', {target: 'http://localhost:3000'}));
+// products = photo carousel
+app.use(proxy('/products', {target: 'http://localhost:4000'}));
+// checkout service
 app.use(proxy('/api/checkout', {target: 'http://localhost:1234'}));
-app.use(proxy('/index', {target: 'http://localhost:4000'}));
+//item description
+app.use(proxy('/api/description', {target: 'http://localhost:3000'}));
 app.use(express.static('./public'));
 
 app.listen(port, () => {
